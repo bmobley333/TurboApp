9fa1bbfe6e8e7e4488dbcfc0540a9a3dfb364c64
bmobley333 <98360038+bmobley333@users.noreply.github.com>
2025-04-07 08:32:11 -0600

    Save Before Morph Roll Refactor Attempt

--- 3.bat
+++ 3.bat
@@ -1,4 +1,4 @@
 copy .\Turbo\*.* .\Text\Turbo\*.txt
 copy .\MSGLib\*.* .\Text\MSGLib\*.txt
-tar -c -a -f "Text\Turbo\TurboFiles.zip" "Turbo"
-tar -c -a -f "Text\MSGLib\MSGLibFiles.zip" "MSGLib"
+powershell Compress-Archive -Path "Turbo\*" -DestinationPath "Text\Turbo\TurboFiles.zip" -Force
+powershell Compress-Archive -Path "MSGLib\*" -DestinationPath "Text\MSGLib\MSGLibFiles.zip" -Force
