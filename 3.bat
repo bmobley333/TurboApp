10743a6e4436e78aba75940b5612722fd28df6e6
bmobley333 <98360038+bmobley333@users.noreply.github.com>
2025-04-07 11:56:27 -0600

    fShowMessage asyn and await works! :)

--- 3.bat
+++ 3.bat
@@ -1,3 +1,5 @@
+del /Q .\Text\Turbo\*.*
+del /Q .\Text\MSGLib\*.*
 copy .\Turbo\*.* .\Text\Turbo\*.txt
 copy .\MSGLib\*.* .\Text\MSGLib\*.txt
 powershell Compress-Archive -Path "Turbo\*" -DestinationPath "Text\Turbo\TurboFiles.zip" -Force
